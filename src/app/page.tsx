8b736ec0a2b2f2563a71c5bc86aca71a315a29c5
c1309047@st.kanazawa-it.ac.jp <c1309047@st.kanazawa-it.ac.jp>
2025-01-27 18:52:30 +0900

    試行錯誤

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@
 
 export default function Home() {
   return (
-    <div><h1>hello</h1></div>
-  );
+  <div className="flex flex-row items-center gap-6">
+    <div>
+      <span>CONCEPT</span>
+      <span>MENU</span>
+      <span>SHOP</span>
+      <span>ACCESS</span>
+    </div>
+  </div>
+    );
 }
